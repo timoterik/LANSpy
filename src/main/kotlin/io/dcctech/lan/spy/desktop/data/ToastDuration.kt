c384de451a4fb92504dc7a5f98d34fa25f59ad3e
ted <69253550+timoterik@users.noreply.github.com>
2023-03-21 21:01:03 +0100

    update Copyright

--- src/main/kotlin/io/dcctech/lan/spy/desktop/data/ToastDuration.kt
+++ src/main/kotlin/io/dcctech/lan/spy/desktop/data/ToastDuration.kt
@@ -1,5 +1,8 @@
 /*
- * A DCCTech © 2022 - 2023 All Rights Reserved. This copyright notice is the exclusive property of DCCTech and is hereby granted to users for use of DCCTech's intellectual property. Any reproduction, modification, distribution, or other use of DCCTech's intellectual property without prior written consent is strictly prohibited. DCCTech reserves the right to pursue legal action against any infringing parties.
+ * A DCCTech © 2022 - 2023 All Rights Reserved. This copyright notice is the exclusive property of DCCTech and
+ * is hereby granted to users for use of DCCTech's intellectual property. Any reproduction, modification, distribution,
+ * or other use of DCCTech's intellectual property without prior written consent is strictly prohibited.
+ *
  */
 
 package io.dcctech.lan.spy.desktop.data
